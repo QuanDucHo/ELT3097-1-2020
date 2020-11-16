aa15594c32e38efe47de4b0c63f650ae892e5edd
mon0811 <lamphamask@gmail.com>
2020-11-16 21:59:22 +0700

    update

---
+++ 68/QuizMath/app/src/main/java/com/nhom68/quizmath/MessageConstant.java
@@ -0,0 +1,8 @@
+package com.nhom68.quizmath;
+
+public class MessageConstant {
+        public static final int UPDATE_QUESTION = 0;
+        public static final int UPDATE_TIMER = 1;
+        public static final int QUESTION_TRUE = 2;
+        public static final int QUESTION_FALSE = 3;
+}
