ed0ab5cbea613b1d6c5646fdc7bd095df3162327
QuanDucHo <rayoptimusprime@gmail.com>
2020-12-06 22:37:53 +0700

    update

--- 68/QuizMath/app/src/main/java/com/nhom68/quizmath/MessageConstant.java
+++
@@ -1,8 +0,0 @@
-package com.nhom68.quizmath;
-
-public class MessageConstant {
-        public static final int UPDATE_QUESTION = 0;
-        public static final int UPDATE_TIMER = 1;
-        public static final int QUESTION_TRUE = 2;
-        public static final int QUESTION_FALSE = 3;
-}
